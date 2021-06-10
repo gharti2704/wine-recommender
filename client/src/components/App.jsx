35c1b462dc627d991fe2eff3ea6876556c32cc6a
Raju Gharti <63073106+gharti2704@users.noreply.github.com>
2021-06-10 18:09:46 -0400

    code refactor

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import View from './View.jsx';
+import React, { useState } from "react";
+import axios from "axios";
+import View from "./View.jsx";
 
 const App = () => {
-
-  const [dish, setDish] = useState('');
+  const [dish, setDish] = useState("");
   const [data, setData] = useState({});
   const [openModal, setOpenModal] = useState(true);
 
   let changeView = () => {
     setOpenModal(false);
-    document.getElementById('form').reset();
+    document.getElementById("form").reset();
   };
 
   let changeHandler = (e) => {
@@ -19,36 +18,30 @@ const App = () => {
 
   let clickHandler = (e) => {
     e.preventDefault();
-    axios.get(`/dish/${dish.toLowerCase()}`)
-      .then(data => {
+    axios
+      .get(`/dish/${dish.toLowerCase()}`)
+      .then((data) => {
         setOpenModal(true);
         setData(data.data);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
 
   return (
     <div className="main">
       <h1>Wine Recommender</h1>
-      <p>Please enter the food to get a pairing for. This can be a dish ("steak"), an ingredient ("salmon"), or a cuisine ("italian").</p>
+      <p>
+        Please enter the food to get a pairing for. This can be a dish
+        ("steak"), an ingredient ("salmon"), or a cuisine ("italian").
+      </p>
       <form action="#" id="form" onSubmit={clickHandler}>
-        <input type="text"
-          onChange={changeHandler}
-          palceholder="Dish name" />
+        <input type="text" onChange={changeHandler} palceholder="Dish name" />
         <button type="submit">Pair</button>
       </form>
 
-      <View
-        data={data}
-        changeView={changeView}
-        openModal={openModal}
-      />
+      <View data={data} changeView={changeView} openModal={openModal} />
     </div>
-
-  )
-
-}
-
-
+  );
+};
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,44 +1,39 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const axios = require('axios');
-
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(__dirname + '/../client/public'));
+app.use(express.static(__dirname + "/../client/public"));
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
-if (process.env.NODE_ENV === 'production') {
-  app.use(morgan('tiny'));
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("tiny"));
 }
 
+app.get("/dish/:name", (req, res) => {
+  axios
+    .get(
+      `/food/wine/pairing?food=${req.params.name}`,
 
-app.get('/dish/:name', (req, res) => {
-
-  axios.get(`/food/wine/pairing?food=${req.params.name}`,
-
-    {
-      baseURL: process.env.API_URL,
-      params: { apiKey: process.env.API_KEY },
-
-    }
-  )
-    .then(data => {
-
+      {
+        baseURL: process.env.API_URL,
+        params: { apiKey: process.env.API_KEY },
+      }
+    )
+    .then((data) => {
       res.json(data.data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.sendStatus(404);
     });
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
